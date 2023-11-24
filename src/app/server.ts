50c6d31e8a7c123c01cd5d8b9f0217d1028e2485
thisis-arman <armanhossen1549@gmail.com>
2023-11-24 18:12:03 +0600

    enviroment setup

---
+++ src/app/server.ts
@@ -0,0 +1,17 @@
+// import { ProcessEnv } from './process.env.d';
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+server().catch((err) => console.log(err));
