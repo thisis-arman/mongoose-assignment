c6940dd3ee4e6497fa54748804d5cfde8c367b53
thisis-arman <armanhossen1549@gmail.com>
2023-11-24 18:42:17 +0600

    users interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
---
+++ src/app/modules/users/user.interface.ts
@@ -0,0 +1,18 @@
+export type users = {
+  userId: "number";
+  username: "string";
+  password: "string";
+  fullName: {
+    firstName: "string";
+    lastName: "string";
+  };
+  age: "number";
+  email: "string";
+  isActive: "boolean";
+  hobbies: ["string", "string"];
+  address: {
+    street: "string";
+    city: "string";
+    country: "string";
+  };
+};
